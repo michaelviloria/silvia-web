52da022e40325281d98c2cfad135401111f3987a
Michael <michaelviloria.g@gmail.com>
2022-01-30 18:28:20 -0500

    Agregado nuevos valor a la navegacion

---
+++ src/main.js
@@ -0,0 +1,13 @@
+const headerNav = document.getElementById("headerNav");
+
+window.onscroll = function () {
+  if(window.scrollY >= 605) {
+    headerNav.classList.remove("no-visible");
+    headerNav.classList.add("show");
+  }
+  else {
+    headerNav.classList.add("no-visible");
+    headerNav.classList.remove("show");
+  }
+  console.log(window.scrollY);
+}
